fac3142bf9f26ecabd17bb0ecc0c60a0911d2af1
Walter Huanquiri <huanquiric@gmail.com>
2026-03-13 15:36:29 -0400

    subiendo java y py

---
+++ Suma.java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+
+public class Suma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir el primer número
+        System.out.print("Ingresa el primer número: ");
+        int num1 = scanner.nextInt();
+
+        // Pedir el segundo número
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        // Sumar los números
+        int suma = num1 + num2;
+
+        // Mostrar el resultado
+        System.out.println("La suma es: " + suma);
+
+        // Cerrar el Scanner
+        scanner.close();
+    }
+}
